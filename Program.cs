bfea080c3a9900f08ec7565bcfdd55fe188cdd75
SponiX <konstantindrozdov960@gmail.com>
2026-05-21 18:22:53 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=app.db"));
+
+var secretKey = builder.Configuration["SecretKey"] ?? "supersecretkey";
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = key
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddLogging();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http:/localhost:5137")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
+
+app.UseCors("AllowFrontend");
+
+
+app.Run();
